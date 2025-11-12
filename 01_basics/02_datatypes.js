c6437acfbf44ea46d6690cb54e92c8ebe22d36f4
sameer608060 <adity.kumar1314@gmail.com>
2025-11-12 18:55:45 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all js code as newer version
+//we are using node js not browser thats why lines like alert(3+3) will throw error
+//js documentation   easy->mdn      original->ecmascript
+ 
+//Data Types
+//number->2 to power 53
+//bigint
+//string-> ""
+//boolean-> True or False
+ //null->standalone
+ //symbol-> Uniqueness
+ //undefind is an object type may be a bug or no reason but it is an object
+ 
+ 
