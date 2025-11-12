f2809ab958e2b63dbd75a89346cff592916a3dc3
sameer608060 <adity.kumar1314@gmail.com>
2025-11-12 18:13:13 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=90542398
+let accountEmail="adity.kumar@gmail.com"
+var accountPass="98348"
+accountcity="patna"
+let accountstate
+//don't use var
+//accountId=2 //not allowed 
+console.table([accountId,accountEmail,accountPass,accountcity,accountstate])
