6c2892b9455dd9f0b862917f0f4c38afda64e028
sameer608060 <adity.kumar1314@gmail.com>
2025-11-24 18:29:51 +0000

    strings basics

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name= new String('Sameer')
+console.log(name)
+const repocount= 5
+console.log(name.indexOf('e'))
+//console.log(`Hello my name is ${name } and my repo count is ${repocount}`)
+
+const newstring="    sameer     "
+console.log(newstring)
+console.log(newstring.trim())
