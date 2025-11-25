88cef2a8fba765629f1238c941f968897c72da40
sameer608060 <adity.kumar1314@gmail.com>
2025-11-25 16:59:33 +0000

    Math_and_num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,14 @@
+const score =400
+const othernumber=23.434
+console.log(score.toString().length)
+console.log(othernumber.toPrecision(4))
+const hundred=1000000000
+
+console.log(hundred.toLocaleString('en-IN'))
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.3))
+console.log(Math.floor(4.3))
+const max=20
+const min=10
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
